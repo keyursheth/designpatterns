5f6f2e07cf49873d5cb3b0845ac239ccde6f223a
keyur sheth <keyursheth3@gmail.com>
2019-09-17 22:36:42 +0530

    added adapter pattern and builder pattern

---
+++ CreationalPatterns/BuilderPattern.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreationalPatterns
+{
+    class BuilderPattern
+    {
+        public static void Main()
+        {
+            IHouseBuilder houseBuilder = new HouseBuilder();
+            houseBuilder.Reset();
+
+            Director director = new Director(houseBuilder);
+            director.BuildSimpleHouse();
+
+            House simpleHouse = houseBuilder.GetHouse();
+
+            houseBuilder.Reset();
+            director.BuildFancyHouse();
+
+            House fancyHouse = houseBuilder.GetHouse();
+
+            Console.WriteLine("Simple House " + simpleHouse.ListParts());
+            Console.WriteLine("Fancy House " + fancyHouse.ListParts());
+
+            Console.ReadLine();
+        }
+    }
+
+
+    public interface IHouseBuilder
+    {
+        void Reset();
+        void BuildWalls();
+        void BuildDoors();
+        void BuildWindows();
+        void BuildGarden();
+        void BuildSwimmingPool();
+        House GetHouse();
+    }
+
+    public class HouseBuilder : IHouseBuilder
+    {
+        private House house;
+
+        public HouseBuilder()
+        {
+            Reset();
+        }
+
+        public void BuildDoors()
+        {
+            house.Add("doors");
+        }
+
+        public void BuildGarden()
+        {
+            house.Add("garden");
+        }
+
+        public void BuildSwimmingPool()
+        {
+            house.Add("swimming pool");
+        }
+
+        public void BuildWalls()
+        {
+            house.Add("walls");
+        }
+
+        public void BuildWindows()
+        {
+            house.Add("windows");
+        }
+
+        public House GetHouse()
+        {
+            return house;
+        }
+
+        public void Reset()
+        {
+            house = new House();
+        }
+    }
+
+
+    public class House
+    {
+        private List<string> _parts = new List<string>();
+
+        public void Add(string part)
+        {
+            _parts.Add(part);
+        }
+
+        public string ListParts()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var part in _parts)
+            {
+                stringBuilder.Append(part + ", ");
+            }
+
+            string partsList = "Parts List : " + stringBuilder.ToString();
+            partsList = partsList.Remove(partsList.Length - 2, 2);
+
+            return partsList;
+        }
+    }
+
+
+
+    public class Director
+    {
+
+        IHouseBuilder _houseBuilder;
+
+        public Director(IHouseBuilder houseBuilder)
+        {
+            _houseBuilder = houseBuilder;
+        }
+
+        public void BuildSimpleHouse()
+        {
+            _houseBuilder.BuildWalls();
+
+            _houseBuilder.BuildDoors();
+
+            _houseBuilder.BuildWindows();
+        }
+
+
+        public void BuildFancyHouse()
+        {
+            _houseBuilder.BuildWalls();
+
+            _houseBuilder.BuildDoors();
+
+            _houseBuilder.BuildWindows();
+
+            _houseBuilder.BuildGarden();
+
+            _houseBuilder.BuildSwimmingPool();
+        }
+    }
+}
---
+++ StructuralPatterns/AdapterPattern.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructuralPatterns
+{
+    public class AdapterPattern
+    {
+        public static void Main()
+        {
+            RoundHole roundHole = new RoundHole();
+            RoundPeg roundPeg = new RoundPeg();
+            roundPeg.Radius = 7;
+
+            SquarePeg squarePeg = new SquarePeg();
+            squarePeg.Width = 9;
+            PegAdapter pegAdapter = new PegAdapter(squarePeg);
+
+            Console.WriteLine("Round Peg fits hole ? : " + roundHole.Fits(roundPeg));
+            Console.WriteLine("Square Peg fits hole ? : " + roundHole.Fits(pegAdapter));
+
+            Console.ReadLine();
+        }
+    }
+
+    public class RoundHole
+    {
+        private const int radius = 7;
+
+        public bool Fits(RoundPeg roundPeg)
+        {
+            return roundPeg.Radius <= radius;
+        }
+    }
+
+    public class RoundPeg
+    {
+        public int Radius { get; set; }
+    }
+
+    public class SquarePeg
+    {
+        public int Width { get; set; }
+    }
+
+    public class PegAdapter : RoundPeg
+    {
+        SquarePeg _squarePeg;
+
+        public PegAdapter(SquarePeg squarePeg)
+        {
+            _squarePeg = squarePeg;
+            Radius = GetRadius();
+        }
+
+        public int GetRadius()
+        {
+            return _squarePeg.Width - 1;
+        }
+    }
+}
