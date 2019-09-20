6ae2e5c7501d4fde8623cade2efae9860b78a939
keyur sheth <keyursheth3@gmail.com>
2019-09-20 07:43:22 +0530

    added bridge and composite pattern

---
+++ StructuralPatterns/BridgePattern.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructuralPatterns
+{
+    public class BridgePattern
+    {
+        public static void Main()
+        {
+            TV tv = new TV();
+            Remote remote = new Remote(tv);
+
+            remote.IncreaseVolume(40);
+            remote.DecreaseVolume(20);
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface IDevice
+    {
+        void SetVolume(int newVolume);
+        void ChangeChanel(int channelNumber);
+    }
+
+    public class Remote
+    {
+        private IDevice _device;
+
+        public Remote(IDevice device)
+        {
+            _device = device;
+        }
+
+        public void IncreaseVolume(int newVolume)
+        {
+            _device.SetVolume(newVolume);
+        }
+
+        public void DecreaseVolume(int newVolume)
+        {
+            _device.SetVolume(newVolume);
+        }
+
+        public void SetNewChannel(int newChannelNo)
+        {
+            _device.ChangeChanel(newChannelNo);
+        }
+    }
+
+    public class TV : IDevice
+    {
+        public void ChangeChanel(int channelNumber)
+        {
+            Console.WriteLine("Channel Set to : " + channelNumber);
+        }
+
+        public void SetVolume(int newVolume)
+        {
+            Console.WriteLine("Volume Set to : " + newVolume);
+        }
+    }
+
+}
---
+++ StructuralPatterns/CompositePattern.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructuralPatterns
+{
+    public class CompositePattern
+    {
+        public static void Main()
+        {
+            Console.WriteLine("I want a leaf");
+            Tree leaf = new Leaf();
+            Console.WriteLine(leaf.CommonOperation());
+
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("I want a branch");
+            Tree branch = GetBranch();
+            Console.WriteLine(branch.CommonOperation());
+
+            Console.ReadLine();
+        }
+
+        public static Branch GetBranch()
+        {
+            Branch branch = new Branch();            
+
+            branch.Add(new Leaf());
+            branch.Add(new Leaf());
+
+            Branch branch1 = new Branch();
+            branch1.Add(new Leaf());
+            branch.Add(branch1);
+
+            return branch;
+        }
+    }
+
+    public abstract class Tree
+    {
+        public virtual void Add(Tree compositeShape)
+        { }
+
+        public virtual void Remove(Tree compositeShape)
+        { }
+
+        public abstract string CommonOperation();
+    }
+
+    public class Leaf : Tree
+    {
+        public override string CommonOperation()
+        {
+            return "Leaf";
+        }
+    }
+
+    public class Branch : Tree
+    {
+        private List<Tree> branches;
+
+        public Branch()
+        {
+            branches = new List<Tree>();
+        }
+        public override string CommonOperation()
+        {
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+
+            sb.Append("Branch(");
+            foreach (var branch in branches)
+            {                
+                sb.Append(branch.CommonOperation());
+                if (i != this.branches.Count - 1)
+                {
+                    sb.Append("+");
+                }
+
+                i++;
+            }
+
+            sb.Append(")");
+            return sb.ToString();
+        }
+
+        public override void Add(Tree tree)
+        {
+            branches.Add(tree);
+        }
+
+        public override void Remove(Tree tree)
+        {
+            branches.Remove(tree);
+        }
+    }
+}
